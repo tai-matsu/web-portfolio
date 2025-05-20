32beaf6531a5aa6f4446cca1c78cba2c92e90bae
Matsusaki <>
2025-05-20 22:46:02 +0900

    stub project pages

---
+++ src/app/projects/luxury/page.tsx
@@ -0,0 +1,3 @@
+export default function Luxury() {
+    return <main className="p-10">Luxury -Coming soon...</main>;
+}
---
+++ src/app/projects/xr/page.tsx
@@ -0,0 +1,3 @@
+export default function XR() {
+    return <main className="p-10">XR指合わせ入力 -Coming soon...</main>;
+}
