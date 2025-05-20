842a8814ef933d766fd2983477af6390e4939b56
Matsusaki <>
2025-05-20 12:20:24 +0900

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <main className = "prose dark:prose-invert mx-auto py-10">
+            <h1>About Me</h1>
+            <p>
+                Taichi Matsusaki | 空間デザイナー &amp; XRクリエイター
+                新しい体験を生み出していく
+            </p>
+        </main>
+    );
+}
