fa8833fb2b86d60cda71c3c7c8a5689b79f7dd9b
Matsusaki <matsusaki2391@gmail.com>
2025-06-17 02:28:36 +0900

    change next_config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/",
-        destination: "/loading",
+        destination: "/preloader",
         permanent: false,
       }
     ]
